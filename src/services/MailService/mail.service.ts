de49fb2601f471a30d51d5ccda09e188655f20b1
Andréas <pinbouen.andreas@gmail.com>
2018-10-06 18:32:24 +0200

    Init Mail Service
    - Adds a service and a watcher in MailService
    - Adds the service to the list of processes of pm2

---
+++ src/services/MailService/mail.service.ts
@@ -0,0 +1,19 @@
+import * as _ from 'lodash';
+
+/**
+ * Service that handles mailing requests.
+ *
+ * @class MailService
+ */
+class MailService {
+
+    /**
+     * Sends mails
+     *
+     * @memberof MailService
+     */
+    sendMail(){
+
+    }
+
+}
