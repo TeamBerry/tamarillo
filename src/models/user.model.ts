c67a4e719c43fe61c90c2918f3d5961600d12778
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2018-10-09 14:17:58 +0200

    Timestamps for User
    - Adds default createdAt & updatedAt timestamps for the user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,28 +3,27 @@ const Video = require('./video.model');
 
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-    name: String,
-    token: String,
-    mail: String,
-    password: String,
-    picture: String,
-    bio: String,
-    followers: [],
-    following: [],
-    moderators: [],
-    friends: [],
-    settings: {
-        color: String,
+var userSchema = new Schema(
+    {
+        name: String,
+        token: String,
+        mail: String,
+        password: String,
+        picture: String,
+        bio: String,
+        followers: [],
+        following: [],
+        moderators: [],
+        friends: [],
+        settings: {
+            color: String,
+        },
+        badges: [],
+        favorites: [{ type: String, ref: 'Video' }],
     },
-    badges: [],
-    favorites: [{ type: String, ref: 'Video' }],
-    joined_at: Date
-});
-
-userSchema.pre('save', (next) => {
-    userSchema.joined_at = new Date();
-    next();
-})
+    {
+        timestamps: true // Will automatically insert createdAt & updatedAt fields
+    }
+);
 
 module.exports = mongoose.model('User', userSchema);
