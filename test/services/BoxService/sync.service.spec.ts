67dc9bb30a1cdf5c9f4fd98cd13ea05f6dc5ccc5
Andréas Pinbouen <angelzatch@gmail.com>
2019-05-30 14:07:09 +0200

    WiP sync service tests

--- test/services/BoxService/sync.service.spec.ts
+++ test/services/BoxService/sync.service.spec.ts
@@ -1,2 +1,50 @@
 var chai = require('chai');
 const expect = chai.expect;
+
+import SyncService from './../../../src/services/BoxService/sync.service';
+const Box = require('../../../src/models/box.schema');
+
+describe("Sync Service", () => {
+
+    before(async () => {
+        await Box.deleteMany(
+            { _id: { $in: ['9cb763b6e72611381ef043e4', '9cb763b6e72611381ef043e5'] } }
+        );
+
+        await Box.create({
+            _id: '9cb763b6e72611381ef043e4',
+            description: null,
+            lang: 'English',
+            name: 'Test box',
+            playlist: [],
+            creator: '9ca0df5f86abeb66da97ba5d',
+            open: true
+        });
+
+        await Box.create({
+            _id: '9cb763b6e72611381ef043e5',
+            description: 'Closed box',
+            lang: 'English',
+            name: 'Closed box',
+            playlist: [],
+            creator: '9ca0df5f86abeb66da97ba5d',
+            open: false
+        });
+    });
+
+    after(async () => {
+        await Box.deleteMany(
+            { _id: { $in: ['9cb763b6e72611381ef043e4', '9cb763b6e72611381ef043e5'] } }
+        );
+    });
+
+    describe("Submit video to box", () => {
+        it("Refuses video if the box is closed", () => {
+
+        });
+
+        it("Accepts the video and sends back the updated box", () => {
+
+        })
+    })
+});
