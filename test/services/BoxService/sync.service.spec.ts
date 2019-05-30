041d571b104fa5b854113cbe937abd15f62365e8
Andréas Pinbouen <angelzatch@gmail.com>
2019-05-30 17:13:02 +0200

    Success test on service.postToBox

--- test/services/BoxService/sync.service.spec.ts
+++ test/services/BoxService/sync.service.spec.ts
@@ -1,8 +1,12 @@
-var chai = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-import SyncService from './../../../src/services/BoxService/sync.service';
+import syncService from './../../../src/services/BoxService/sync.service';
 const Box = require('../../../src/models/box.schema');
+const User = require('../../../src/models/user.model');
+const Video = require('../../../src/models/video.model');
 
 describe("Sync Service", () => {
 
@@ -10,6 +14,17 @@ describe("Sync Service", () => {
         await Box.deleteMany(
             { _id: { $in: ['9cb763b6e72611381ef043e4', '9cb763b6e72611381ef043e5'] } }
         );
+
+        await User.findByIdAndDelete('9ca0df5f86abeb66da97ba5d');
+
+        await Video.findByIdAndDelete('9cb81150594b2e75f06ba8fe');
+
+        await User.create({
+            _id: '9ca0df5f86abeb66da97ba5d',
+            name: 'Ash Ketchum',
+            mail: 'ash@pokemon.com',
+            password: 'Pikachu'
+        })
 
         await Box.create({
             _id: '9cb763b6e72611381ef043e4',
@@ -30,21 +45,44 @@ describe("Sync Service", () => {
             creator: '9ca0df5f86abeb66da97ba5d',
             open: false
         });
+
+        await Video.create({
+            _id: '9cb81150594b2e75f06ba8fe',
+            link: 'Ivi1e-yCPcI',
+            name: 'Destroid - Annihilate'
+        });
     });
 
     after(async () => {
         await Box.deleteMany(
             { _id: { $in: ['9cb763b6e72611381ef043e4', '9cb763b6e72611381ef043e5'] } }
         );
+
+        await User.findByIdAndDelete('9ca0df5f86abeb66da97ba5d');
+
+        await Video.findByIdAndDelete('9cb81150594b2e75f06ba8fe');
     });
 
-    describe("Submit video to box", () => {
-        it("Refuses video if the box is closed", () => {
+    describe.only("Submit video to box", () => {
+        const video = {
+            _id: '9cb81150594b2e75f06ba8fe',
+            link: 'Ivi1e-yCPcI',
+            name: 'Destroid - Annihilate'
+        };
 
+        it("Refuses video if the box is closed", async () => {
+            const call = await syncService.postToBox(video, '9cb763b6e72611381ef043e5', '9ca0df5f86abeb66da97ba5d')
+
+            console.log(call);
+
+            expect(call).to.eventually.be.rejectedWith('This box is closed. Submission is disallowed.');
         });
 
-        it("Accepts the video and sends back the updated box", () => {
+        it("Accepts the video and sends back the updated box", async () => {
+            const updatedBox = await syncService.postToBox(video, '9cb763b6e72611381ef043e4', '9ca0df5f86abeb66da97ba5d');
 
-        })
+            console.log(updatedBox.playlist);
+            expect(updatedBox.playlist.length).to.eql(1);
+        });
     })
 });
