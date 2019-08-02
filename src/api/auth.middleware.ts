2421e6da221f0c2519ede705aa6d407f812a20b2
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-08-02 18:11:56 +0200

    Update Auth middleware

--- src/api/auth.middleware.ts
+++ src/api/auth.middleware.ts
@@ -1,21 +1,22 @@
 import { NextFunction, Request, Response } from "express"
 
 import * as jwt from "jsonwebtoken"
-const fs = require("fs")
-const RSA_PRIVATE_KEY = fs.readFileSync("certs/private_key.pem")
 
 module.exports.isAuthorized = (request: Request, response: Response, next: NextFunction) => {
     console.log("Auth middleware check...")
     const auth = request.headers.authorization
     if (auth) {
-        // console.log('PLAYLISTS. AUTH: ', request.headers.authorization)
         try {
-            const token = jwt.verify(auth, RSA_PRIVATE_KEY)
+            const tokenArray = request.headers.authorization.split(" ")
+
+            jwt.verify(tokenArray[1], process.env.JWT_PASS, { algorithm: "HS256" })
+
             console.log("Access granted.")
-            console.log(token)
+
             return next()
         } catch (error) {
             console.log("Access refused: ", error)
+
             return response.status(401).send("UNAUTHORIZED")
         }
     }
