d1fc07b581d05904e0a825ca0ef47a0d57e547c0
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2019-11-13 17:50:18 +0100

    Use Amazon SES to send mails

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-Deploy:
+# Setup
 
+## Amazon SES
+- The file to create is here: ~/.aws/credentials
+
+## Deploy
+
+```
 scp -r dist/ package.json processes.json root@berrybox.tv:berrybox/Chronos/
 rm -rf node_modules npm-shrinkwrap.json
 npm restart
+```
--- test/services/MailService/mail.service.spec.ts
+++ test/services/MailService/mail.service.spec.ts
@@ -16,7 +16,7 @@ describe.skip("MailService", () => {
     describe("Test mail send", () => {
         it("Sends a signup mail", async () => {
             await mailService.sendMail({
-                addresses: ['pinbouen.andreas@gmail.com'],
+                addresses: ['success@simulator.amazonses.com'],
                 variables: {},
                 template: 'signup'
             });
@@ -24,7 +24,7 @@ describe.skip("MailService", () => {
 
         it("Sends a reset password mail", async () => {
             await mailService.sendMail({
-                addresses: ['pinbouen.andreas@gmail.com'],
+                addresses: ['success@simulator.amazonses.com'],
                 variables: {
                     resetToken: '35d76s813ez1s3C7f3dsf1s'
                 },
