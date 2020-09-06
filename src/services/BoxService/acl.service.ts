fdf05d174422064c7fdbfa0bc4c1f31980e10832
AngelZatch <angelzatch@gmail.com>
2020-09-06 12:14:16 +0200

    chore: update code with muscadine

--- src/services/BoxService/acl.service.ts
+++ src/services/BoxService/acl.service.ts
@@ -1,15 +1,10 @@
 import { RoleChangeRequest } from "../../models/role-change.model"
 import { Subscriber, PopulatedSubscriberDocument } from "../../models/subscriber.model"
-import { FeedbackMessage, BoxScope, Permission } from "@teamberry/muscadine"
+import { FeedbackMessage, BoxScope, Permission, ACLEvaluationResult } from "@teamberry/muscadine"
 const Box = require("../../models/box.model")
 
-export enum ACLResult {
-    NO = 0,
-    YES = 1,
-    BERRIES = 2
-}
 class ACLService {
-    public async isAuthorized(scope: BoxScope, action: Permission): Promise<ACLResult> {
+    public async isAuthorized(scope: BoxScope, action: Permission): Promise<ACLEvaluationResult> {
         const subscriber = await Subscriber.findOne({
             userToken: scope.userToken,
             boxToken: scope.boxToken
@@ -17,21 +12,21 @@ class ACLService {
 
         // Admin powers bypass everything
         if (subscriber.role === 'admin') {
-            return ACLResult.YES
+            return ACLEvaluationResult.YES
         }
 
         const box = await Box.findById(scope.boxToken).lean()
 
         if (box.acl[subscriber.role].includes(action)) {
-            return ACLResult.YES
+            return ACLEvaluationResult.YES
         } else {
             // If berries are enabled
             if (box.options.berries){
                 const berriesPermissions: Array<Permission> = ['forceNext', 'forcePlay', 'skipVideo']
-                return berriesPermissions.includes(action) ? ACLResult.BERRIES : ACLResult.NO
+                return berriesPermissions.includes(action) ? ACLEvaluationResult.BERRIES : ACLEvaluationResult.NO
             }
         }
-        return ACLResult.NO
+        return ACLEvaluationResult.NO
     }
 
     // Step 1: update the role of the target subscriber
