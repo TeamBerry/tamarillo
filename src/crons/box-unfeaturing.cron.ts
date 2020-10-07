24746fc670c8cc5a8bcb8a20d7218c932c4b5518
AngelZatch <angelzatch@gmail.com>
2020-10-07 20:36:59 +0200

    feat: unfeaturing cron

---
+++ src/crons/box-unfeaturing.cron.ts
@@ -0,0 +1,33 @@
+require("./../config/connection")
+const BoxSchema = require("./../models/box.model")
+
+export class BoxUnfeaturingCron {
+    public async process(): Promise<void> {
+        await this.run()
+    }
+
+    public async run(): Promise<void> {
+        console.log('Hey! Starting scan of boxes to unfeature...')
+
+        const boxesToUnfeature = await BoxSchema.find({ featured: { $lte: new Date('2022-01-01T00:00:00Z') } })
+
+        console.log(`${boxesToUnfeature.length} boxes to unfeature`)
+
+        for (const box of boxesToUnfeature) {
+            await BoxSchema.findByIdAndUpdate(
+                box._id,
+                {
+                    $set: { featured: null }
+                }
+            )
+        }
+
+        console.log(`${boxesToUnfeature.length} boxes have been unfeatured. Operation complete.`)
+
+        process.exit(0)
+    }
+}
+
+const boxUnfeaturingCron = new BoxUnfeaturingCron()
+void boxUnfeaturingCron.process()
+export default boxUnfeaturingCron
