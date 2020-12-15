7ec37bcf9724be08092f890dc9944f323a4d8c1c
AngelZatch <angelzatch@gmail.com>
2020-12-15 15:13:31 +0100

    feat: migrations & create badge

--- src/api/routes/badge.api.ts
+++ src/api/routes/badge.api.ts
@@ -1,5 +1,8 @@
-import { Request, Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
+import * as _ from "lodash"
+
 import { Badge } from '../../models/badge.model'
+const auth = require("./../middlewares/auth.middleware")
 
 export class BadgeApi {
     public router: Router
@@ -11,11 +14,34 @@ export class BadgeApi {
 
     public init(): void {
         this.router.get("/", this.index)
+
+        this.router.use(auth.isAuthorized)
+        this.router.post('/', this.store)
     }
 
     public async index(request: Request, response: Response): Promise<Response> {
         const badges = await Badge.find({})
         return response.status(200).send(badges)
+    }
+
+    public async store(request: Request, response: Response): Promise<Response> {
+        const decodedToken = response.locals.auth
+
+        if (!decodedToken.physalis) {
+            return response.status(401).send('UNAUTHORIZED')
+        }
+
+        try {
+            if (_.isEmpty(request.body)) {
+                return response.status(412).send('MISSING_PARAMETERS')
+            }
+
+            const createdBadge = await Badge.create(request.body)
+
+            return response.status(201).send(createdBadge)
+        } catch (error) {
+            return response.status(500).send(error)
+        }
     }
 }
 
--- test/api/routes/badge.api.spec.ts
+++ test/api/routes/badge.api.spec.ts
@@ -2,13 +2,19 @@ import * as bodyParser from 'body-parser'
 import * as chai from 'chai'
 import * as express from 'express'
 import * as supertest from 'supertest'
+import authService from '../../../src/api/services/auth.service'
 const expect = chai.expect
 
 import { Badge, BadgeDocument } from '../../../src/models/badge.model'
+import { User } from '../../../src/models/user.model'
 import BadgeApi from './../../../src/api/routes/badge.api'
+import { Session } from "./../../../src/models/session.model"
 
 describe("Badge API", () => {
     const expressApp = express()
+
+    let ashJWT: Session = null
+    let adminJWT: Session = null
 
     before(async () => {
         expressApp.use(bodyParser.json({ limit: '15mb', type: 'application/json' }))
@@ -23,6 +29,23 @@ describe("Badge API", () => {
                 availableTo: '2020-12-31T23:59:59Z'
             }
         ])
+
+        const ashUser = await User.create({
+            _id: '9ca0df5f86abeb66da97ba5d',
+            name: 'Ash Ketchum',
+            mail: 'ash@pokemon.com',
+            password: 'Pikachu',
+        })
+
+        const adminUser = await User.create({
+            _id: '9ca0df5f86abeb66da97ba60',
+            name: 'Admin',
+            mail: 'admin@berrybox.com',
+            password: 'Berrybox'
+        })
+
+        ashJWT = authService.createSession(ashUser)
+        adminJWT = authService.createSession(adminUser)
     })
 
     after(async () => {
@@ -37,6 +60,44 @@ describe("Badge API", () => {
                 const badges: Array<BadgeDocument> = response.body
 
                 expect(badges).to.have.lengthOf(1)
+        })
+    })
+
+    describe("Create a badge", () => {
+        it("Fails if the user does not have the credentials", () => {
+            return supertest(expressApp)
+                .post('/')
+                .set('Authorization', `Bearer ${ashJWT.bearer}`)
+                .send({
+                    name: '1 Year Anniversary',
+                    picture: 'https://berrybox-badges.s3-eu-west-1.amazonaws.com/1-yr.png',
+                    description: 'For your 1 year of Berrybox',
+                    availableFrom: null,
+                    availableTo: null
+                })
+                .expect(401, 'UNAUTHORIZED')
+        })
+
+        it("Fails if the body is empty", () => {
+            return supertest(expressApp)
+                .post('/')
+                .set('Authorization', `Bearer ${adminJWT.bearer}`)
+                .send()
+                .expect(412)
+        })
+
+        it("Creates the badge", () => {
+            return supertest(expressApp)
+                .post('/')
+                .set('Authorization', `Bearer ${adminJWT.bearer}`)
+                .send({
+                    name: '1 Year Anniversary',
+                    picture: 'https://berrybox-badges.s3-eu-west-1.amazonaws.com/1-yr.png',
+                    description: 'For your 1 year of Berrybox',
+                    availableFrom: null,
+                    availableTo: null
+                })
+                .expect(201)
         })
     })
 })
