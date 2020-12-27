4ee28cc4e100720504a981513dc4602867ad2ff6
AngelZatch <angelzatch@gmail.com>
2020-12-27 12:25:59 +0100

    refacto: clean badge event handling

--- src/services/BadgeService/badge.service.ts
+++ src/services/BadgeService/badge.service.ts
@@ -3,6 +3,8 @@ import { BadgeEvent } from '../../models/badge.job'
 import { Badge } from '../../models/badge.model'
 import { User } from '../../models/user.model'
 const badgeQueue = new Queue('badges')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const mongoose = require("./../../config/connection")
 
 class BadgeService {
     public listen() {
@@ -17,51 +19,60 @@ class BadgeService {
     public async handleEvent(data: BadgeEvent) {
         const { subject, userToken } = data
 
-        // Get user and their badges
-        const targetUser = await User.findById(userToken)
-        const targetUserBadges = targetUser.badges.map(badge => badge.badge.toString())
+        try {
+            // Get user and their badges
+            const targetUser = await User.findById(userToken) ?? null
 
-        // Get badges matching the event that the user doesn't have in their collection
-        const possibleBadges = await Badge.find(
-            {
-                'unlockConditions.key': subject.key,
-                "_id": { $nin: [targetUserBadges] },
-                "$and": [
-                    {
-                        $or: [
-                            { availableFrom: null },
-                            { availableFrom: {$lte: new Date()}}
-                        ]
-                    },
-                    {
-                        $or: [
-                            { availableTo: null },
-                            { availableTo: {$gte: new Date()}}
-                        ]
-                    }
-                ]
+            if (!targetUser) {
+                return
             }
-        ).lean()
 
-        // Guard against the number of badges that can be awarded
-        if (possibleBadges.length === 0) {
-            return targetUser
+            const targetUserBadges = targetUser.badges.map(badge => badge.badge.toString())
+
+            // Get badges matching the event that the user doesn't have in their collection
+            const possibleBadges = await Badge.find(
+                {
+                    'unlockConditions.key': subject.key,
+                    "_id": { $nin: [targetUserBadges] },
+                    "$and": [
+                        {
+                            $or: [
+                                { availableFrom: null },
+                                { availableFrom: {$lte: new Date()}}
+                            ]
+                        },
+                        {
+                            $or: [
+                                { availableTo: null },
+                                { availableTo: {$gte: new Date()}}
+                            ]
+                        }
+                    ]
+                }
+            ).lean()
+
+            // Guard against the number of badges that can be awarded
+            if (possibleBadges.length === 0) {
+                return targetUser
+            }
+
+            // If a badge matches, award it to the user
+            possibleBadges.forEach(badge => {
+                if (
+                    (badge.unlockConditions.valueType === 'string' && subject.value === badge.unlockConditions.value)
+                        || (badge.unlockConditions.valueType === 'number' && subject.value >= badge.unlockConditions.value)
+                ) {
+                    targetUser.badges.push({
+                        badge: badge._id.toString(),
+                        unlockedAt: new Date()
+                    })
+                }
+            })
+
+            return targetUser.save()
+        } catch (error) {
+            console.log('error: ', error)
         }
-
-        // If a badge matches, award it to the user
-        possibleBadges.forEach(badge => {
-            if (
-                (badge.unlockConditions.valueType === 'string' && subject.value === badge.unlockConditions.value)
-                    || (badge.unlockConditions.valueType === 'number' && subject.value >= badge.unlockConditions.value)
-            ) {
-                targetUser.badges.push({
-                    badge: badge._id.toString(),
-                    unlockedAt: new Date()
-                })
-            }
-        })
-
-        return targetUser.save()
     }
 }
 
